d0866a6e4426388ae7e6b21a635d39f232f76e26
dani <ddaann07@gmail.com>
2018-11-14 16:46:13 +0530

    minor changes

--- app/helpers/shopify_helper.rb
+++ app/helpers/shopify_helper.rb
@@ -8,7 +8,7 @@ module ShopifyHelper
 
 	def access_shopify relative_url,http_method='get'
 		url = APP_CONFIG["shopify_domain"] + relative_url
-		options = {:user_name => "support@schoolay.com",:password => "9524732613"}
+		options = {:user_name => APP_CONFIG["shopify_username"],:password => APP_CONFIG["shopify_password"]}
 		resp = http_request url,{}, options,http_method
 		JSON.parse resp[1] if resp[0] 
 	end
